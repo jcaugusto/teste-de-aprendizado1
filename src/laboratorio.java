2c8edb6ad9fa1491bae3baf14b532994d95db099
João Claudio Augusto de Oliveira <103705723+jcaugusto@users.noreply.github.com>
2023-12-17 21:08:01 -0300

    first commit

---
+++ src/laboratorio.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class laboratorio {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int  hora;
+		
+		System.out.println("QUANTAS HORAS?");
+		hora =sc.nextInt();
+		
+		if ( hora  < 12) {
+			System.out.println("bom dia");
+		}
+		
+		else if (hora < 18) {
+			System.out.println("boa tarde!");
+			}
+			
+			
+		else {
+				System.out.println("boa noite!");
+			}
+			
+			
+		
+		
+		
+		
+		sc.close();
+	
+	}
+
+}
